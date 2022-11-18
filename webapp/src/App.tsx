35b25f256cc1c233d807e97df7105ba2675e61e4
Leon Windheuser <leon.windheuser@gmail.com>
2022-11-19 00:06:30 +0100

    Add tiktum logo

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tiktum.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+           Watch your Lecture in a modern way
         </p>
         <a
           className="App-link"
