29a63a225cb2ec128457f4903bc6d61fedcc6371
Leon Windheuser <leon.windheuser@gmail.com>
2022-11-19 20:39:25 +0100

    Fix merge

--- webapp/src/components/content.tsx
+++ webapp/src/components/content.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, Component, FormEvent} from "react";
+import React, {ChangeEvent, Component, FormEvent, useRef, useState} from "react";
 import styles from "../styles/mediacard.module.css";
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import Snowfall from "react-snowfall";
@@ -7,34 +7,20 @@ import fireEmoji from "../static/fire-emoji.png"
 import joyEmoji from "../static/joy-emoji.png"
 import shitEmoji from "../static/shit-emoji.png"
 
-interface State {
-    shitCount: number,
-    fireCount: number,
-    joyCount: number,
-}
-
 type Props = {
     video: string,
 }
 
-export class MediaCard extends Component<Props, State> {
+export const MediaCard = ({video}: Props) => {
+        const shit: CanvasImageSource = document.createElement('img')
+        shit.src = shitEmoji;
+        const joy: CanvasImageSource = document.createElement('img')
+        joy.src = joyEmoji;
+        const fire: CanvasImageSource = document.createElement('img')
+        fire.src = fireEmoji;
 
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            shitCount: 0,
-            fireCount: 0,
-            joyCount: 0,
-        }
-    }
-
-    componentDidMount() {
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    private handleClick(emoji: string) {
-        const shit = document.createElement('img')
+        const onEmojiyClick = (emoji: string) => {
+             const shit = document.createElement('img')
             shit.src = "../static/shit-emoji.png";
             const joy = document.createElement('img')
             joy.src = "../static/joy-emoji.png";
@@ -43,61 +29,65 @@ export class MediaCard extends Component<Props, State> {
 
             switch (emoji) {
                 case "fire": {
-                    this.setState({fireCount: this.state.fireCount + 15});
-                    setTimeout(() => this.setState({fireCount: 0}), 5000);
+                    setFireCount(fireCount + 15);
+                    setTimeout(() => setFireCount(0), 5000);
                     break;
                 }
                 case "shit": {
-                    this.setState({shitCount: this.state.shitCount + 15});
-                    setTimeout(() => this.setState({shitCount: 0}), 5000);
+                    setShitCount(shitCount + 15);
+                    setTimeout(() => setShitCount(0), 5000);
                     break;
                 }
                 case "joy": {
-                    this.setState({joyCount: this.state.joyCount + 15});
-                    setTimeout(() => this.setState({joyCount: 0}), 5000);
+                    setJoyCount(joyCount + 15);
+                    setTimeout(() => setJoyCount(0), 5000);
                     break;
                 }
             }
-    }
+        }
 
+        const [shitCount, setShitCount] = useState(0);
+        const [fireCount, setFireCount] = useState(0);
+        const [joyCount, setJoyCount] = useState(0);
 
-    render() {
-        const shit: CanvasImageSource = document.createElement('img')
-        shit.src = shitEmoji;
-        const joy: CanvasImageSource = document.createElement('img')
-        joy.src = joyEmoji;
-        const fire: CanvasImageSource = document.createElement('img')
-        fire.src = fireEmoji;
-
+        const videoRef = useRef<any>();
+        const handleChange = () => {
+            if (videoRef !== null) {
+                if (videoRef.current.paused) {
+                    videoRef.current.play();
+                } else {
+                    videoRef.current.pause();
+                }
+            }
+        }
         return (
             <div className={styles.body}>
-                <video controls className={styles.video}>
-                    <source src={this.props.video} type="video/mp4" className={styles.video}/>
+                <video className={styles.video} onClick={handleChange} ref={videoRef}>
+                    <source src={video} type="video/mp4" className={styles.video}/>
                 </video>
 
                 <div className={styles.emojis}>
                     <AddCommentIcon/>
-                    <div onClick={() => this.handleClick("fire")}>🔥</div>
-                    <div onClick={() => this.handleClick("joy")}>😂</div>
-                    <div onClick={() => this.handleClick("shit")}>💩</div>
+                    <div onClick={() => onEmojiyClick("fire")}>🔥</div>
+                    <div onClick={() => onEmojiyClick("joy")}>😂</div>
+                    <div onClick={() => onEmojiyClick("shit")}>💩</div>
                 </div>
 
                 <Snowfall
-                  snowflakeCount={this.state.shitCount}
+                  snowflakeCount={shitCount}
                   radius={[30.0, 30.0]}
                   // Pass in the images to be used
                   images={[shit]}/>
 
                 <Snowfall
-                  snowflakeCount={this.state.fireCount}
+                  snowflakeCount={fireCount}
                   radius={[30.0, 30.0]}
                   images={[fire]}/>
 
                 <Snowfall
-                  snowflakeCount={this.state.joyCount}
+                  snowflakeCount={joyCount}
                   radius={[30.0, 30.0]}
                   images={[joy]}/>
             </div>
         );
-    }
 }
